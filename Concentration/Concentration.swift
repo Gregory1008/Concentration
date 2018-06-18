c3cf8987cc0c1784f68963e4842c7a8b149d7889
Roland Herzfeld <rolandherzfeld@rolands-imac.fritz.box>
2018-06-18 15:52:28 +0200

    uniquified

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -55,3 +55,11 @@ extension Collection {
         return count == 1 ? first : nil
     }
 }
+
+extension Array where Element: Equatable {
+    var uniquified: [Element] {
+        var elements = [Element]()
+        forEach { if !elements.contains($0) { elements.append($0) } }
+        return elements
+    }
+}
